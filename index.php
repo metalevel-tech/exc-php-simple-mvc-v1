90e24b4a906cddcddae73db9948955dadf46395a
pa4080 <spas.z.spasov@gmail.com>
2022-09-30 14:36:53 +0300

    Initial commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <?php
 
 spl_autoload_register(function ($class_name) {
-    require_once './classes/'. $class_name .'.php';
+    require_once('classes/'. $class_name .'.php');
 });
 
 require_once ('Routes.php');
